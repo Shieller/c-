276cd60411279a2e2c7de66481490e96331f55f5
Leo <lennartvaraksin@gmail.com>
2022-08-09 17:42:44 +0300

    add_if_else

---
+++ example003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("ВВедите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура маша");
+
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
