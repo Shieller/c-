8a944aa1d9a737871476fdb1c3067ebec56ba768
Leo <lennartvaraksin@gmail.com>
2022-08-31 12:03:35 +0300

    Dz3

--- example002/Program.cs
+++ example002/Program.cs
@@ -1,7 +1,17 @@
-﻿Console.WriteLine("Введите число ");
-string? numberstring = Console.ReadLine();
-int n = int.Parse(numberstring!);
-for (int i=1; i < n+1; i++){
-    Console.WriteLine(i*i*i);
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+// В задании написанно массив из 8-ми элеменов а в приерах разные размерности, сделал как в примерах 
+
+Console.WriteLine("Введите кол-во элементов массива");
+var col = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[col];
+for (int i = 0; i < col; i++)
+{
+    array[i] = new Random().Next(0, 50);
 }
- 
+for (int i = 0; i < col; i++)
+{
+    Console.WriteLine(array[i]);
+}
