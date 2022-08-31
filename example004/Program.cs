2e8fa9a23f2a7e865ebc28e41f96c8200267aa0e
Leo <lennartvaraksin@gmail.com>
2022-08-31 11:26:44 +0300

    Dz2

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,7 +1,14 @@
-﻿Console.WriteLine("Введите пятизначное число");
-string? n = Console.ReadLine();
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
 
-  if (n?[0]==n?[4] && n?[1]==n?[3]){
-    Console.WriteLine("число палиндром.");
-  }
-  else Console.WriteLine("не палиндром.");
+Console.WriteLine("Введите число");
+var n = int.Parse(Console.ReadLine()!);
+int c = 0;
+while (n > 0)
+{
+    c = c + (n % 10);
+    n = n / 10;
+}
+Console.WriteLine(c);
