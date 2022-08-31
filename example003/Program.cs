0ee423e3935bb727a755e83d71371c1fa16123c5
Leo <lennartvaraksin@gmail.com>
2022-08-31 10:53:02 +0300

    dz1

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,17 +1,14 @@
 ﻿
-Console.WriteLine("Введите цифру от 1 до 7");
-string? numberstring = Console.ReadLine();
-int n = int.Parse(numberstring!);
+Console.WriteLine("Введите число которое хотите возвести в степень");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень в котору надо возвести");
+int y = int.Parse(Console.ReadLine()!);
 
-if(n>5 & n<8)
+int result = x;
+
+for (int i = 1; i < y; i++)
 {
-Console.WriteLine("Да");
+    result = result * x;
 }
-else if (n<5 & n>0)
-{
-   Console.WriteLine("Нет"); 
-}
-else
-{
-    Console.WriteLine("Неправельный ввод");
-}
+Console.WriteLine(result);
+
