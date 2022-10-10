e6a528cb1ed0e609a351f3344f4863397e63c625
Leo <lennartvaraksin@gmail.com>
2022-10-10 20:10:35 +0300

    Dz1

---
+++ dz1.3.py
@@ -0,0 +1,10 @@
+
+import math
+print('Введите x1 и y1')
+x1=float(input())
+y1=float(input())
+print('Введите x2 и y2')
+x2=float(input())
+y2=float(input())
+result= math.sqrt(pow((x2-x1),2)+pow((y2-y1),2))
+print (result)
